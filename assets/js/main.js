2322351aa0c163b1fd74713540085db9d3036231
AntonioSabella <asabella6@gmail.com>
2022-03-26 12:18:02 +0100

    Add css and js adjusts

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,35 +15,39 @@ generateButtonElement.addEventListener('click', function () {
   //Chiedere all'utente il nome, il numero di chilometri che vuole percorrere e l'età.
   
   const fullName = fullNameInputElement.value;
-  const age = ageSelectElement.value;
   const km_to_do = distanceInputElement.value;
-
-  console.log(fullName, km_to_do, age);
+  const age = ageSelectElement.value;
+  //console.log(fullName, km_to_do, age);
 
   //Calcolo del prezzo totale del viaggio, a seconda che sia quello standard oppure soggetto a sconto del 20% 0 del 40%
 
   let ticket_price = km_to_do * 0.21
-  console.log(ticket_price);
+  //console.log(ticket_price);
 
 
-  if (isNaN(km_to_do)) {
-    console.log("Attenzione: devi inserire valori numerici");
-  } else if (age == 'minorenne') {
-    ticket_price = ticket_price - (ticket_price * 0.2)
-    info = `${fullName}Ecco a te il 20% di sconto, giovane Skywalker. <br> Prezzo biglietto stellare: ${ticket_price.toFixed(2)}`
+  /* if (isNaN(km_to_do)) {
+    //console.log("Attenzione: devi inserire valori numerici");
+  } passaggio superfluo, in quanto ho modificato il type dell'input da "text" a "number" */
+  
+  if (age == 'minorenne') {
+    ticket_price = ticket_price - (ticket_price * 0.2);
+    info = `${fullName} Ecco a te il 20% di sconto, giovane Skywalker. <br> Prezzo biglietto stellare: ${ticket_price.toFixed(2)} ${"€"}`;
+    console.log(info);
     inform = `Sconto del 20% giovane Skywalker`
     //console.log(ticket_price);
     //console.log(info);
 
   } else if (age == 'over65') {
     ticket_price = ticket_price - (ticket_price * 0.4)
-    info = `${fullName}Saggio Yoda, per i tuoi servigi meriti uno sconto del 40%. <br> Prezzo biglietto stellare: ${ticket_price.toFixed(2)}`
+    info = `${fullName} Saggio Yoda, per i tuoi servigi meriti uno sconto del 40%. <br> Prezzo biglietto stellare: ${ticket_price.toFixed(2)} ${"€"}`
+    console.log(info);
     inform = `Sconto del 40% Saggio Yoda`
     //console.log(ticket_price);
     //console.log(info);
 
   } else {
-    info = `${fullName}Per te Stormtrooper vale il prezzo per intero. Prezzo pieno del biglietto stellare: ${ticket_price.toFixed(2)}`
+    info = `${fullName} Per te Stormtrooper vale il prezzo per intero. Prezzo pieno del biglietto stellare: ${ticket_price.toFixed(2)} ${"€"}`
+    console.log(info);
     inform = `Spiacente! <br> Prezzo pieno caro Stormtrooper😎`
     //console.log(ticket_price);
     //console.log(info);
@@ -58,12 +62,12 @@ generateButtonElement.addEventListener('click', function () {
   document.querySelector('.offer_type').innerHTML = inform;
   document.querySelector('.carrozza').innerHTML = carrozza;
   document.querySelector('.cp_code').innerHTML = cp_code;
-  document.querySelector('.ticket_price').innerHTML = ticket_price.toFixed(2);
+  document.querySelector('.ticket_price').innerHTML = ticket_price.toFixed(2) + `${" €"}`;
 
 });
 
 
-// Il pulsante annulla rimuove tutte le informazioni dalla pagina
+// Il pulsante annulla rimuove tutte le informazioni dalla pagina per un ulteriore inserimento
 cancelButtonElement.addEventListener('click', function () {
   fullNameInputElement.value = ""
   ageSelectElement.value = "minorenne"
